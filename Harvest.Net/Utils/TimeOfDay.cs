7ef95ce0858032cf3eb83f50952e899d51ba6299
Ivan Sukhikh <mailcannon@gmail.com>
2018-05-31 16:46:06 +0700

    TimeOfDay - harvest time parser

--- Harvest.Net/Utils/TimeOfDay.cs
+++ Harvest.Net/Utils/TimeOfDay.cs
@@ -1,23 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace Harvest.Net.Utils
 {
     public class TimeOfDay
     {
-        private TimeSpan 
+        private readonly DateTime _time;
+        private readonly CultureInfo _usCulture;
 
-        public TimeOfDay(string time)
+        public TimeOfDay(string time) : this()
         {
+            var dateTime = DateTime.Parse(time);
+            _time = dateTime;
+        }
 
-            
+        public TimeOfDay(DateTime time) : this()
+        {
+            _time = time;
+        }
+
+        private TimeOfDay()
+        {
+            _usCulture = CultureInfo.CreateSpecificCulture("en-US");
         }
 
         public override string ToString()
         {
-            var jsonTimeString = time.ToString("hh:mmtt", _usCulture); // It will give "03:00AM"
-            writer.WriteValue(jsonTimeString);
+            var jsonTimeString = _time.ToString("h:mmtt", _usCulture); // It will give "3:00AM"
+            return jsonTimeString;
         }
     }
 }
